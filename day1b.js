2a5fe0d136fe31a9e578ebebdcf45d0d74c77fa1
sy54321 <steve@Stephens-Mac-mini.local>
2018-12-08 18:13:50 -0600

    remove async from function that didn't need it, rearrange slightly other

--- day1b.js
+++ day1b.js
@@ -1,13 +1,14 @@
 const fs = require('fs');
 const util = require('util');
-let fileName = 'dataDay1.txt';
-
 // Convert fs.readFile into Promise version of same    
 const readFile = util.promisify(fs.readFile);
 
+
+let fileName = 'dataDay1.txt';
 let currentFrequency = 0
 let seenFrequencies = []
 let foundMatch = false
+
 async function getFile() {
   return await readFile(fileName);
 }
@@ -15,13 +16,7 @@ async function getFile() {
 // Can't use `await` outside of an async function so you need to chain
 // with then()
 getFile().then(data => {
-    myData = (data.toString());
-    myData = (myData.split('\n'));
-    let intList = [];
-    for (i of myData) {
-        intList.push(Number(i));
-    }
-    async function searchList() {
+    function searchList() {
         for (i=0; i< intList.length; i++) {
             seenFrequencies.push(currentFrequency);
             currentFrequency += intList[i];
@@ -31,6 +26,12 @@ getFile().then(data => {
                 break;
             }
         }
+    }
+    myData = (data.toString());
+    myData = (myData.split('\n'));
+    let intList = [];
+    for (i of myData) {
+        intList.push(Number(i));
     }
     while (foundMatch == false) {
         searchList();
